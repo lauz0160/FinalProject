bcf328f8b79cceacb175f5b8821dab8083a41698
lauz0160 <lauz0160@algonquinlive.com>
2020-03-05 10:02:33 -0500

    SETUP FOR PROJECT

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btnImage = findViewById(R.id.btnImageOfDay);
+      //  btnImage.setOnClickListener(btn -> startActivity(new Intent(MainActivity.this, MainActivity.class)));
+
+        Button btnGuardian = findViewById(R.id.btnGuardian);
+      //  btnImage.setOnClickListener(btn -> startActivity(new Intent(MainActivity.this, MainActivity.class)));
+
+        Button btnEarth = findViewById(R.id.btnEarthImage);
+     //   btnImage.setOnClickListener(btn -> startActivity(new Intent(MainActivity.this, MainActivity.class)));
+
+        Button btnBBC = findViewById(R.id.btnBBC);
+     //   btnImage.setOnClickListener(btn -> startActivity(new Intent(MainActivity.this, MainActivity.class)));
+
     }
 }
